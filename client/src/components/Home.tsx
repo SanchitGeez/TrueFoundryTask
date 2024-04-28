74120fc1494f435a141f928d1a12f82b3014801c
Sanchit Gupta <sanchit.gupta@hiwipay.com>
2024-04-28 12:57:31 +0530

    build changes

--- client/src/components/Home.tsx
+++ client/src/components/Home.tsx
@@ -5,27 +5,16 @@ const Home:React.FC = () => {
   return (
     <div>
       <Link to="/chat">
-        <button style={buttonStyle}>CHAT</button>
+        <button className='buttonStyle'>CHAT</button>
       </Link>
       <Link to="/dashboard">
-        <button style={buttonStyle}>DASHBOARD</button>
+        <button className='buttonStyle'>DASHBOARD</button>
       </Link>
       <Link to="/stats">
-        <button style={buttonStyle}>USER STATS</button>
+        <button className='buttonStyle'>USER STATS</button>
       </Link>
     </div>
   );
-};
-
-const buttonStyle = {
-  backgroundColor: '#333', // Dark grey color
-  color: '#fff',  
-  width:"10%",         // White text color
-  padding: '10px 20px',
-  border: 'none',
-  borderRadius: '5px',
-  cursor: 'pointer',
-  margin:'15px'
 };
 
 export default Home;
